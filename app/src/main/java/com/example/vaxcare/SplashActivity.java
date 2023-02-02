ad88319145ad6605459841a6426acc218ea2ca17
Hussain <hus98ain@gmail.com>
2023-02-02 14:29:37 +0500

    v1.0

---
+++ app/src/main/java/com/example/vaxcare/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.vaxcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
